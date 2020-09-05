d087a733ae5c605c4fa9729631cfa27f00d31fe8
alphagogo <26833525+alphagot@users.noreply.github.com>
2020-09-05 15:26:49 +0800

    submit

--- python/装饰器.py
+++ python/装饰器.py
@@ -1,2 +1,31 @@
 # __author:  xiaoxinpro13
 # date:  2020/8/15
+import time
+
+
+def foo():
+    time.sleep(1)
+    print('foo')
+
+
+def logit(fn):
+    def inner():
+        print('---')
+        fn()
+        print('***')
+    return inner
+
+
+foo = logit(foo)
+
+foo()
+
+
+#语法糖
+
+@logit
+def mark():
+    time.sleep(2)
+    print('mark')
+
+mark()
